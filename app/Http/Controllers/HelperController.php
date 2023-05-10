a588e2ec6f6b389218202ef2ca159d8175616b6c
ABE <abe@abewartech.github.io>
2023-05-10 16:36:17 +0700

    🐛 request not found

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -9,19 +9,19 @@ class HelperController extends Controller
     public function aisdata()
     {
         // Create new Vessel
-        $vessel = Vessel::create($request->only((new Vessel)->getFillable()));
+        $vessel = Vessel::create(request()->only((new Vessel)->getFillable()));
 
         // Create new Port
-        $port = Port::create($request->only((new Port)->getFillable()));
+        $port = Port::create(request()->only((new Port)->getFillable()));
 
         // Create new Position
-        $position = Position::create($request->only((new Position)->getFillable()));
+        $position = Position::create(request()->only((new Position)->getFillable()));
 
         // Create new VesselPort
         $vesselPort = new VesselPort([
             'vessel_id' => $vessel->id,
             'port_id' => $port->id,
-            'visit_date' => $request->visit_date,
+            'visit_date' => request()->visit_date,
         ]);
         $vesselPort->save();
 
