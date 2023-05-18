7567282d287422c627783cea7672cb8974b50be3
ABE <abe@abewartech.github.io>
2023-05-18 17:33:01 +0700

    ✨ facit

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,43 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx',
+            input: "public/frontend/src/index.tsx",
             refresh: true,
         }),
         react(),
     ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return `vendor-${
+                            id
+                                .toString()
+                                .split("/node_modules/")[1]
+                                .split("/")[0]
+                        }`;
+                    }
+                },
+            },
+            optimization: {
+                splitChunks: {
+                    cacheGroups: {
+                        vendor: {
+                            test: /[\\/]node_modules[\\/]/,
+                            name: "vendor",
+                            chunks: "all",
+                            enforce: true,
+                        },
+                    },
+                },
+            },
+        },
+        chunkSizeWarningLimit: 5800,
+        reportCompressedSize: false,
+    },
 });
