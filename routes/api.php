2b00023453e56e07fcdc2b6674d8414968fc6760
ABE <abe@abewartech.github.io>
2023-05-30 14:09:05 +0700

    ✨ api login & regis

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,25 @@
 <?php
 
 use App\Http\Controllers\DataloggerController;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\HelperController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/login', [AuthenticatedSessionController::class, 'store']);
+Route::post('/register', [RegisteredUserController::class, 'store']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//dari sensor
 Route::post('aisdata', [HelperController::class, 'aisdata']);
 
+//CRUD
 Route::apiResource('dataloggers', DataloggerController::class);
+
+//FE
 Route::get('aisdata', [HelperController::class, 'getaisdata']);
 Route::get('aisdataunique', [HelperController::class, 'aisdataunique']);
