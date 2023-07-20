0680bae6da03346e4bdcf03b2644df95dc3b11de
abe <yusufarifin89@gmail.com>
2023-07-20 10:24:23 +0700

    ✨ out of range

--- database/migrations/2023_05_21_071651_ais_data_vessels.php
+++ database/migrations/2023_05_21_071651_ais_data_vessels.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->string('dimension_to_port')->nullable();
             $table->string('dimension_to_starboard')->nullable();
             $table->string('reported_destination')->nullable();
+            $table->enum('out_of_range', ['0', '1'])->default('0');
             $table->timestamp('reported_eta')->nullable();
             $table->timestamps();
 
