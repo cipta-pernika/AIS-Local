b12b9662f9b34917c2ba17cf00d741ddfc50a563
Rahmad Al Habib <yusufarifin89@gmail.com>
2023-07-28 15:42:30 +0700

    🐛

--- DOCS/git_pull.py
+++ DOCS/git_pull.py
@@ -4,6 +4,7 @@ import schedule
 import time
 
 repository_path = r'C:\Users\USER\Documents\AIS-Local'
+composer_path = r'C:\ProgramData\ComposerSetup\bin\composer.bat'
 
 def git_pull():
     try:
@@ -11,12 +12,12 @@ def git_pull():
         os.chdir(repository_path)
 
         # Execute the Git pull command using subprocess
-        subprocess.check_output(['git', 'pull'])
-        print('Git pull successful!')
+        git_output = subprocess.run(['git', 'pull'], capture_output=True, text=True, check=True)
+        print('Git pull output:', git_output.stdout)
 
         # Execute the composer install command using subprocess
-        subprocess.check_output(['composer', 'install'])
-        print('Composer install successful!')
+        composer_output = subprocess.run([composer_path, 'install'], capture_output=True, text=True, check=True)
+        print('Composer install output:', composer_output.stdout)
 
     except subprocess.CalledProcessError as e:
         print('Error during Git pull or Composer install:', e)
@@ -24,10 +25,10 @@ def git_pull():
 # Perform the initial Git pull and Composer install
 git_pull()
 
-# Schedule periodic Git pulls and Composer installs every 10 minutes
-schedule.every(10).minutes.do(git_pull)
+# # Schedule periodic Git pulls and Composer installs every 10 minutes
+# schedule.every(10).minutes.do(git_pull)
 
-# Keep the script running
-while True:
-    schedule.run_pending()
-    time.sleep(60)
+# # Keep the script running
+# while True:
+#     schedule.run_pending()
+#     time.sleep(60)
--- DOCS/git_pullfe.py
+++ DOCS/git_pullfe.py
@@ -4,6 +4,7 @@ import schedule
 import time
 
 repository_path = r'C:\Users\USER\Documents\trackerFe'
+yarn_path = r'C:\Users\USER\AppData\Roaming\npm\node_modules\yarn\bin\yarn.cmd'  # Replace with the actual path to the yarn executable
 
 def git_pull():
     try:
@@ -15,7 +16,7 @@ def git_pull():
         print('Git pull successful!')
 
         # Execute the yarn install command using subprocess
-        subprocess.check_output(['yarn', 'install'])
+        subprocess.check_output([yarn_path, 'install'])
         print('Yarn install successful!')
 
     except subprocess.CalledProcessError as e:
@@ -25,9 +26,10 @@ def git_pull():
 git_pull()
 
 # Schedule periodic Git pulls and Yarn installs every 10 minutes
-schedule.every(10).minutes.do(git_pull)
+# schedule.every(10).minutes.do(git_pull)
 
-# Keep the script running
-while True:
-    schedule.run_pending()
-    time.sleep(60)
+# # Keep the script running and print the timestamp for each scheduled run
+# while True:
+#     schedule.run_pending()
+#     print('Scheduled job executed at:', time.strftime('%Y-%m-%d %H:%M:%S'))
+#     time.sleep(60)
