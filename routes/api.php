d66be1365969c9faf9aa78fb317c0aa3aaaa807e
bhisma6 <141414018+arbaidi@users.noreply.github.com>
2023-08-08 20:01:10 +0700

    🐛

--- routes/api.php
+++ routes/api.php
@@ -114,3 +114,4 @@ Route::post('deletelocation', [LocationController::class, 'deletelocation']);
 
 Route::post('dailyreport', [HelperController::class, 'dailyreport']);
 Route::get('dailyreportpdf', [HelperController::class, 'dailyreportprint']);
+Route::post('dailyreportpdf', [HelperController::class, 'dailyreportprint']);
