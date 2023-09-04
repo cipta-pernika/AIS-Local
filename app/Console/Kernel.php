710fd007eb1d9b3f11667163cca18833bbce8ef7
ABE <abeabeabeabe444@gmail.com>
2023-09-04 10:45:42 +0700

    ✨ php artisan schedule:run

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('fetchradar')->daily();
+        $schedule->command('fetchradar')->everyMinute();
         $schedule->command('app:outofrange')->daily();
         $schedule->command('radarpng')->daily();
         $schedule->command('app:sendata')->daily();
