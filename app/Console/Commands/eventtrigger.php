10fef1f9fc7c4dcbbcb8a9083e8470dcdfb46cfc
ABE <abeabeabeabe444@gmail.com>
2023-10-03 13:03:47 +0700

    ✨ ON/OFF AIS Receiver Coverage

--- app/Console/Commands/eventtrigger.php
+++ app/Console/Commands/eventtrigger.php
@@ -6,6 +6,8 @@ use App\Models\AisDataPosition;
 use App\Models\Asset;
 use App\Models\Datalogger;
 use App\Models\EventTracking;
+use Location\Coordinate;
+use Location\Distance\Vincenty;
 use Illuminate\Console\Command;
 
 class eventtrigger extends Command
@@ -37,15 +39,8 @@ class eventtrigger extends Command
 
         $datalogger = Datalogger::first();
 
-        //$datalogger has lat & long
-
         $allAssets = Asset::with('aisDataVessel')->get();
 
-        // ON/OFF AIS Receiver Coverage
-        // the coverage is from $datalogger->coverage
-        // if asset position (AisDataPosition) outside coverage then create EventTracking with event_id 5 else 4
-
-        // AIS ON/OFF
         foreach ($allAssets as $asset) {
             if ($asset->aisDataVessel) {
                 $vesselId = $asset->aisDataVessel->id;
@@ -55,35 +50,44 @@ class eventtrigger extends Command
                     ->first();
 
                 // Check AIS Receiver Coverage
-                $latTerakhirKapal = $latestAisData->latitude;
-                $lonTerakhirKapal = $latestAisData->longitude;
-                $coverage = $datalogger->coverage;
+                if ($latestAisData) {
 
-                if ($this->isPositionOutsideCoverage($latTerakhirKapal, $lonTerakhirKapal, $datalogger, $coverage)) {
-                    EventTracking::create([
-                        'asset_id' => $asset->id,
-                        'event_id' => 5,
-                        'ais_data_position_id' => $latestAisData->id,
-                    ]);
-                    echo "Asset {$asset->asset_name}: AIS Receiver Coverage OFF\n";
-                } else {
-                    // AIS Receiver Coverage is ON, continue with AIS status check
-                    echo "Asset {$asset->asset_name}: AIS Receiver Coverage ON\n";
+                    $latTerakhirKapal = $latestAisData->latitude;
+                    $lonTerakhirKapal = $latestAisData->longitude;
 
-                    // Check the AIS status directly instead of comparing it with a string
-                    if ($aisStatus) {
-                        // AIS is ON, insert record into event_trackings
+                    // ON/OFF AIS Receiver Coverage
+                    if ($this->isPositionOutsideCoverage($latTerakhirKapal, $lonTerakhirKapal, $datalogger)) {
                         EventTracking::create([
-                            'asset_id' => $asset->id, // Replace with the actual asset_id
-                            'event_id' => 2,
-                            'ais_data_position_id' => $latestAisData->id, // Use $vesselId instead of $aisStatus->id
+                            'asset_id' => $asset->id,
+                            'event_id' => 5,
+                            'ais_data_position_id' => $latestAisData->id,
                         ]);
+                        echo "Asset {$asset->asset_name}: AIS Receiver Coverage OFF\n";
                     } else {
+                        // AIS Receiver Coverage is ON, continue with AIS status check
+                        echo "Asset {$asset->asset_name}: AIS Receiver Coverage ON\n";
                         EventTracking::create([
-                            'asset_id' => $asset->id, // Replace with the actual asset_id
-                            'event_id' => 3,
-                            'ais_data_position_id' => $latestAisData->id, // Set to null or handle accordingly
+                            'asset_id' => $asset->id,
+                            'event_id' => 4,
+                            'ais_data_position_id' => $latestAisData->id,
                         ]);
+
+                        // AIS ON/OFF
+                        // Check the AIS status directly instead of comparing it with a string
+                        if ($aisStatus) {
+                            // AIS is ON, insert record into event_trackings
+                            EventTracking::create([
+                                'asset_id' => $asset->id, // Replace with the actual asset_id
+                                'event_id' => 2,
+                                'ais_data_position_id' => $latestAisData->id, // Use $vesselId instead of $aisStatus->id
+                            ]);
+                        } else {
+                            EventTracking::create([
+                                'asset_id' => $asset->id, // Replace with the actual asset_id
+                                'event_id' => 3,
+                                'ais_data_position_id' => $latestAisData->id, // Set to null or handle accordingly
+                            ]);
+                        }
                     }
                 }
             } else {
@@ -93,10 +97,14 @@ class eventtrigger extends Command
     }
 
     // Function to check if the position is outside coverage
-    function isPositionOutsideCoverage($latKapal, $lonKapal, $datalogger, $coverage)
+    function isPositionOutsideCoverage($latKapal, $lonKapal, $datalogger)
     {
-        // Implement your logic to check if the position is outside coverage
-        // Compare $position with $coverage and return true if outside coverage, false otherwise
-        // Example: return $position < $coverage['min_lat'] || $position > $coverage['max_lat'] || $position < $coverage['min_long'] || $position > $coverage['max_long'];
+        $coordinate1 = new Coordinate($datalogger->latitude, $datalogger->longitude);
+        $coordinate2 = new Coordinate($latKapal, $lonKapal);
+        $distance = number_format($coordinate1->getDistance($coordinate2, new Vincenty()), 0, 0, 0) * 1;
+
+        $coverageInMeters = $datalogger->coverage * 1852;
+
+        return $distance > $coverageInMeters;
     }
 }
