c4fe9efb7454ff95ee24610d88cc7b7e35a600da
ABE <abeabeabeabe444@gmail.com>
2023-12-06 19:19:47 +0700

    ✨

---
+++ app/Filament/Resources/DataTransferLogResource/Pages/CreateDataTransferLog.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\DataTransferLogResource\Pages;
+
+use App\Filament\Resources\DataTransferLogResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateDataTransferLog extends CreateRecord
+{
+    protected static string $resource = DataTransferLogResource::class;
+}
---
+++ app/Filament/Resources/LocationTypeResource.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LocationTypeResource\Pages;
+use App\Filament\Resources\LocationTypeResource\RelationManagers;
+use App\Models\LocationType;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LocationTypeResource extends Resource
+{
+    protected static ?string $model = LocationType::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    protected static ?string $navigationGroup = 'Location';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('description')
+                    ->maxLength(255),
+                Forms\Components\TextInput::make('icon')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('description')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('icon')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLocationTypes::route('/'),
+            'create' => Pages\CreateLocationType::route('/create'),
+            'edit' => Pages\EditLocationType::route('/{record}/edit'),
+        ];
+    }
+}
