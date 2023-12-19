2003a4d398ae519d198412e5f1635ea353407f55
ABE <abeabeabeabe444@gmail.com>
2023-12-19 16:44:22 +0700

    🐛

--- app/Filament/Resources/ReportGeofenceResource.php
+++ app/Filament/Resources/ReportGeofenceResource.php
@@ -78,7 +78,7 @@ class ReportGeofenceResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                // Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
