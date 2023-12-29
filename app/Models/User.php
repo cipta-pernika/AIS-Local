9961dbfb9e5c714f2ef727b07029ef4ce226f9e7
ABE <abeabeabeabe444@gmail.com>
2023-12-29 09:32:05 +0700

    🐛

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,7 +13,7 @@ use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, HasRoles, HasPanelShield;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
