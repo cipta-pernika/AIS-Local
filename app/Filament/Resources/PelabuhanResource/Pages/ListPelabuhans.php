db8ca9e835209f236f375cab7d409a053d6e8e1b
ABE <abeabeabeabe444@gmail.com>
2024-01-16 15:34:08 +0700

    🐛

--- app/Filament/Resources/PelabuhanResource/Pages/ListPelabuhans.php
+++ app/Filament/Resources/PelabuhanResource/Pages/ListPelabuhans.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\PelabuhanResource\Pages;
 use App\Filament\Resources\PelabuhanResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Konnco\FilamentImport\Actions\ImportField;
+use Konnco\FilamentImport\Actions\ImportAction;
 
 class ListPelabuhans extends ListRecords
 {
@@ -14,6 +16,18 @@ class ListPelabuhans extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+            ImportAction::make()
+                ->fields([
+                    ImportField::make('name'),
+                    ImportField::make('latitude'),
+                    ImportField::make('longitude'),
+                    ImportField::make('penanggung_jawab'),
+                    ImportField::make('no_izin_pengoperasian'),
+                    ImportField::make('tgl_izin_pengoperasian'),
+                    ImportField::make('penerbit_izin_pengoperasian'),
+                    ImportField::make('no_perjanjian_sewa_perairan'),
+                    ImportField::make('tgl_sewa_perairan'),
+                ])
         ];
     }
 }
