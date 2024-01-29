769f6a77926847984caa7ea6c66be8355397e97a
ABE <abeabeabeabe444@gmail.com>
2024-01-29 18:54:13 +0700

    🐛

--- app/Filament/Widgets/AisDataChart.php
+++ app/Filament/Widgets/AisDataChart.php
@@ -4,20 +4,52 @@ namespace App\Filament\Widgets;
 
 use App\Models\AisData;
 use App\Models\AisDataPosition;
+use Carbon\Carbon;
 use Filament\Widgets\ChartWidget;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
 
 class AisDataChart extends ChartWidget
 {
-    protected static ?string $heading = 'Chart';
+    use InteractsWithPageFilters;
+    protected static ?string $heading = 'AIS DATA';
+
+    public function getDescription(): ?string
+    {
+        return 'Diagram Statistik AIS Data';
+    }
 
     protected function getData(): array
     {
+        $startDate = filled($this->filters['startDate'] ?? null) ?
+            \Illuminate\Support\Carbon::parse($this->filters['startDate']) :
+            now()->subYear();
+
+        $endDate = filled($this->filters['endDate'] ?? null) ?
+            \Illuminate\Support\Carbon::parse($this->filters['endDate']) :
+            now();
+
+        // $pelabuhanId = $this->filters['pelabuhan_id'] ?? null;
+
+        // $query = AisDataPosition::query()
+        //     ->when($pelabuhanId, function ($query) use ($pelabuhanId) {
+        //         $query->whereHas('sensorData.sensor', function ($subquery) use ($pelabuhanId) {
+        //             $subquery->where('datalogger_id', $pelabuhanId);
+        //         });
+        //     })
+        //     ->whereBetween('created_at', [$startDate, $endDate]);
+
+        // $data = $query->selectRaw('COUNT(*) as aggregate, DATE_FORMAT(created_at, "%Y-%m") as date')
+        //     ->groupBy('date')
+        //     ->get();
+
+        // $data = collect($data);
+
         $data = Trend::model(AisDataPosition::class)
             ->between(
-                start: now()->subYear(),
-                end: now(),
+                start: $startDate,
+                end: $endDate,
             )
             ->perMonth()
             ->count();
--- app/Filament/Widgets/VesselTypeOverview.php
+++ app/Filament/Widgets/VesselTypeOverview.php
@@ -10,10 +10,18 @@ class VesselTypeOverview extends BaseWidget
 {
     protected function getStats(): array
     {
+        $startDate = filled($this->filters['startDate'] ?? null) ?
+            \Illuminate\Support\Carbon::parse($this->filters['startDate']) :
+            now()->subYear();
+
+        $endDate = filled($this->filters['endDate'] ?? null) ?
+            \Illuminate\Support\Carbon::parse($this->filters['endDate']) :
+            now();
+
         return [
-            Stat::make('Fishing', AisDataVessel::query()->where('vessel_type', 'Fishing')->count()),
-            Stat::make('Tug', AisDataVessel::query()->where('vessel_type', 'Tug')->count()),
-            Stat::make('Cargo', AisDataVessel::query()->where('vessel_type', 'Cargo')->count()),
+            Stat::make('Fishing', AisDataVessel::query()->where('vessel_type', 'Fishing')->whereBetween('created_at', [$startDate, $endDate])->count()),
+            Stat::make('Tug', AisDataVessel::query()->where('vessel_type', 'Tug')->whereBetween('created_at', [$startDate, $endDate])->count()),
+            Stat::make('Cargo', AisDataVessel::query()->where('vessel_type', 'Cargo')->whereBetween('created_at', [$startDate, $endDate])->count()),
         ];
     }
 }
