02c118bb097b2ea9b0bfb62da886b64042f851a8
ABE <abeabeabeabe444@gmail.com>
2024-01-31 16:35:35 +0700

    🐛 pulse

--- config/database.php
+++ config/database.php
@@ -63,6 +63,26 @@ return [
             ]) : [],
         ],
 
+        'pulse' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST_PULSE', '127.0.0.1'),
+            'port' => env('DB_PORT_PULSE', '3306'),
+            'database' => env('DB_DATABASE_PULSE', 'forge'),
+            'username' => env('DB_USERNAME_PULSE', 'forge'),
+            'password' => env('DB_PASSWORD_PULSE', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
