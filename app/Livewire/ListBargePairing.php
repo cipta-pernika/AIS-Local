a67e2ad49b79f6691edf93c2fdc88011a7298f10
ABE <abeabeabeabe444@gmail.com>
2024-02-23 13:30:10 +0700

    🐛

--- app/Livewire/ListBargePairing.php
+++ app/Livewire/ListBargePairing.php
@@ -2,14 +2,21 @@
 
 namespace App\Livewire;
 
+use App\Models\AisDataVessel;
 use App\Models\InaportnetPergerakanKapal;
 use Livewire\Component;
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Enums\ActionsPosition;
 use Filament\Tables\Contracts\HasTable;
 use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Actions\Action;
+use Filament\Forms\Components\Select;
+use Illuminate\Database\Eloquent\Model;
+use Filament\Tables\Filters\SelectFilter;
 use Illuminate\Contracts\View\View;
 
 class ListBargePairing extends Component implements HasForms, HasTable
@@ -23,13 +30,116 @@ class ListBargePairing extends Component implements HasForms, HasTable
             ->query(InaportnetPergerakanKapal::query())
             ->columns([
                 TextColumn::make('no_pkk'),
+                TextColumn::make('aisDataVessel.mmsi')
+                    ->label('MMSI')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('nama_kapal')
+                    ->searchable(),
+                TextColumn::make('jenis_layanan')
+                    ->searchable(),
+                TextColumn::make('nama_negara')
+                    ->searchable(),
+                TextColumn::make('tipe_kapal')
+                    ->searchable()->action(
+                        Action::make('assign')->icon('heroicon-m-pencil-square')
+                            ->button()
+                            ->badgeColor('success')
+                            ->label('Assign')
+                            ->labeledFrom('md')
+                            ->form([
+                                Select::make('no_pkk_assign')
+                                    ->label('No PKK')
+                                    ->options(AisDataVessel::query()->whereNotNull('no_pkk')->pluck('no_pkk', 'no_pkk'))
+                                    ->required(),
+                            ])
+                            ->action(function (array $data, InaportnetPergerakanKapal $record): void {
+                                $record->no_pkk_assign = $data['no_pkk_assign'];
+                                $record->update();
+                            })
+                    ),
+                TextColumn::make('nama_perusahaan')
+                    ->searchable(),
+                TextColumn::make('tgl_tiba')
+                    ->searchable(),
+                TextColumn::make('tgl_brangkat')
+                    ->searchable(),
+                TextColumn::make('bendera')
+                    ->searchable(),
+                TextColumn::make('gt_kapal')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('dwt')
+                    ->numeric()
+                    ->sortable(),
+                TextColumn::make('no_imo')
+                    ->searchable(),
+                TextColumn::make('call_sign')
+                    ->searchable(),
+                TextColumn::make('nakhoda')
+                    ->searchable(),
+                TextColumn::make('jenis_trayek')
+                    ->searchable(),
+                TextColumn::make('pelabuhan_asal')
+                    ->searchable(),
+                TextColumn::make('pelabuhan_tujuan')
+                    ->searchable(),
+                TextColumn::make('lokasi_lambat_labuh')
+                    ->searchable(),
+                TextColumn::make('waktu_respon')
+                    ->searchable(),
+                TextColumn::make('nomor_spog')
+                    ->searchable(),
             ])
             ->filters([
-                // ...
+                SelectFilter::make('tipe_kapal')
+                    ->options([
+                        'TONGKANG / BARGE' => 'TONGKANG / BARGE',
+                        'KAPAL MOTOR TUNDA (TUG BOAT)' => 'KAPAL MOTOR TUNDA (TUG BOAT)'
+                    ]),
             ])
+            ->filtersTriggerAction(
+                fn (Action $action) => $action
+                    ->button()
+                    ->label('Filter'),
+            )
             ->actions([
-                // ...
-            ])
+                Action::make('assign')->icon('heroicon-m-pencil-square')
+                    ->button()
+                    ->hidden(function (Table $table, Model $record) {
+                        // Check if no_pkk_assign is not null
+                        if ($record->no_pkk_assign !== null) {
+                            return true; // Hide the action
+                        }
+
+                        if ($record->tipe_kapal !== 'TONGKANG / BARGE') {
+                            return true; // Hide the action
+                        }
+
+                        return false; // Show the action
+                    })
+                    ->badgeColor('success')
+                    ->label('Assign')
+                    ->labeledFrom('md')
+                    ->form([
+                        Select::make('assignId')
+                            ->label('No PKK')
+                            ->required()
+                            ->relationship(
+                                name: 'assignId',
+                                modifyQueryUsing: fn (Builder $query) => $query->orderBy('no_pkk')->orderBy('vessel_name'),
+                            )
+                            ->getOptionLabelFromRecordUsing(fn (Model $record) => "{$record->no_pkk} ~ {$record->vessel_name} ~ {$record->nama_perusahaan}")
+                            ->searchable(['no_pkk', 'vessel_name', 'nama_perusahaan'])
+                    ])
+                    ->action(function (array $data, InaportnetPergerakanKapal $record): void {
+                        $record->no_pkk_assign = $data['no_pkk_assign'];
+                        $record->update();
+                    })
+                    ->fillForm(fn (InaportnetPergerakanKapal $record): array => [
+                        'no_pkk_assign' => $record->no_pkk_assign,
+                    ])
+            ], position: ActionsPosition::BeforeColumns)
             ->bulkActions([
                 // ...
             ]);
