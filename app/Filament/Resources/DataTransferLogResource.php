bae7c315039cee795902c5dee0110e5948920519
ABE <abeabeabeabe444@gmail.com>
2024-02-26 10:10:33 +0700

    🐛

--- app/Filament/Resources/DataTransferLogResource.php
+++ app/Filament/Resources/DataTransferLogResource.php
@@ -17,7 +17,7 @@ class DataTransferLogResource extends Resource
 {
     protected static ?string $model = DataTransferLog::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-queue-list';
 
     protected static ?string $navigationGroup = 'Logs';
 
