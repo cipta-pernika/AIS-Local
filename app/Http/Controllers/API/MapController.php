ff0930ba5fbf33e2fd6bfbc4ed8f5be848294bbe
ABE <abeabeabeabe444@gmail.com>
2024-02-29 10:49:04 +0700

    🐛

--- app/Http/Controllers/API/MapController.php
+++ app/Http/Controllers/API/MapController.php
@@ -20,7 +20,7 @@ class MapController extends Controller
 
         if ($ais_vessel) {
             $ais_position = AisDataPosition::where('vessel_id', $ais_vessel->id)
-                ->with('reportGeofences', 'reportGeofences.geofence', 'reportGeofences.geofence.pelabuhan', 'reportGeofences.geofence.location')
+                ->with('reportGeofence', 'reportGeofence.geofence', 'reportGeofence.geofence.pelabuhan', 'reportGeofence.geofence.location')
                 ->orderBy('created_at', 'DESC')->first();
         }
 
