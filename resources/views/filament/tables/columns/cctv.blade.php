b5d244c2449c2dc32ebc18faced535093078c8d4
ABE <abeabeabeabe444@gmail.com>
2024-03-04 17:55:59 +0700

    🐛

--- resources/views/filament/tables/columns/cctv.blade.php
+++ resources/views/filament/tables/columns/cctv.blade.php
@@ -1,5 +1,5 @@
 <div class="p-2">
-    <a href="https://unair.ac.id/wp-content/uploads/2020/04/pelabuhan.jpg" target="_blank">
-        <img class="w-32" src="https://unair.ac.id/wp-content/uploads/2020/04/pelabuhan.jpg" />
+    <a href="{{$getState()}}" target="_blank">
+        <img class="w-32" src="{{$getState()}}" />
     </a>
 </div>
