ba1378deceeaa4b5b80a675144a8145e67a95cf4
ABE <abeabeabeabe444@gmail.com>
2024-03-07 16:48:00 +0700

    🐛

--- app/Filament/Resources/CameraCaptureResource.php
+++ app/Filament/Resources/CameraCaptureResource.php
@@ -10,6 +10,7 @@ use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -43,7 +44,9 @@ class CameraCaptureResource extends Resource
                     ->sortable(),
                 Tables\Columns\TextColumn::make('geofence.geofence_name')
                     ->sortable(),
-                Tables\Columns\ViewColumn::make('image')->view('filament.tables.columns.image'),
+                // Tables\Columns\ViewColumn::make('image')->view('filament.tables.columns.image'),
+                ImageColumn::make('image')
+                    ->disk('minio'),
                 Tables\Columns\TextColumn::make('created_at')
                     ->dateTime()
                     ->sortable()
