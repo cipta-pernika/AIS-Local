e2f19bcd553c7a33099129b59ebcd22ad756e121
ABE <abeabeabeabe444@gmail.com>
2024-03-13 16:48:55 +0700

    🐛

--- resources/views/pdf/datamandiripdf.blade.php
+++ resources/views/pdf/datamandiripdf.blade.php
@@ -2,68 +2,403 @@
 <html>
 
 <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <title>Daily Report</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+        * {
+            margin: 0;
+            padding: 0;
+            text-indent: 0;
+        }
+
+        p {
+            color: black;
+            font-family: Arial, sans-serif;
+            font-style: normal;
+            font-weight: bold;
+            text-decoration: none;
+            font-size: 17.5pt;
+            margin: 0pt;
+        }
+
+        .s1 {
+            color: black;
+            font-family: Arial, sans-serif;
+            font-style: normal;
+            font-weight: bold;
+            text-decoration: none;
+            font-size: 8.5pt;
+        }
+
+        h1 {
+            color: black;
+            font-family: Arial, sans-serif;
+            font-style: normal;
+            font-weight: bold;
+            text-decoration: none;
+            font-size: 10.5pt;
+        }
+
+        .s2 {
+            color: black;
+            font-family: Arial, sans-serif;
+            font-style: normal;
+            font-weight: normal;
+            text-decoration: none;
+            font-size: 8.5pt;
+        }
+
+        table,
+        tbody {
+            vertical-align: top;
+            overflow: visible;
+        }
+
+        .data-mandiri {
+            padding-top: 3pt;
+            padding-left: 8pt;
+            text-indent: 0pt;
+            text-align: left;
+        }
+
+        .border1-td {
+            width: 100pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border2-td {
+            width: 242pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border3-td {
+            width: 231pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border4-td {
+            width: 71pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border5-td {
+            width: 100pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border6-td {
+            width: 89pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border7-td {
+            width: 66pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border8-td {
+            width: 97pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border9-td {
+            width: 68pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border10-td {
+            width: 26pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border11-td {
+            width: 140pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border12-td {
+            width: 120pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border13-td {
+            width: 150pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+
+        .border14-td {
+            width: 156pt;
+            border-top-style: solid;
+            border-top-width: 1pt;
+            border-left-style: solid;
+            border-left-width: 1pt;
+            border-bottom-style: solid;
+            border-bottom-width: 1pt;
+            border-right-style: solid;
+            border-right-width: 1pt;
+        }
+    </style>
 </head>
 
 <body>
-    <div>
-        <h1 class="mt-4">Data Mandiri</h1>
-
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>Total Kapal</th>
-                    <th>Passing</th>
-                    <th>Pandu</th>
-                    <th>Bongkar Muat</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>{{ $summaryData['passing_count'] + $summaryData['pandu_count'] + $summaryData['bongkar_muat_count'] }}</td>
-                    <td><span class="badge bg-primary">{{ $summaryData['passing_count'] }}</span></td>
-                    <td><span class="badge bg-success">{{ $summaryData['pandu_count'] }}</span></td>
-                    <td><span class="badge bg-danger">{{ $summaryData['bongkar_muat_count'] }}</span></td>
-                </tr>
-            </tbody>
-        </table>
-
-
-        <h2 class="mt-4">List Kapal</h2>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>No</th>
-                    <th>Informasi Kapal</th>
-                    <th>No PKK</th>
-                    <th>Passing</th>
-                    <th>Pandu</th>
-                    <th>Bongkar Muat</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($addons as $key => $addon)
-                <tr>
-                    <td>{{ $key + 1 }}</td>
-                    <td>
-                        Nama Kapal: {{ $addon->aisDataVessel->vessel_name }} <br>
-                        Tipe: {{ $addon->aisDataVessel->vessel_type }} <br>
-                        MMSI: {{ $addon->aisDataVessel->mmsi }} <br>
-                        Perusahaan: {{ $addon->aisDataVessel->nama_perusahaan }}
-                    </td>
-                    <td>{{ $addon->aisDataVessel->no_pkk }}</td>
-                    <td>{{ $addon->isPassing ? 'Yes' : '' }}</td>
-                    <td>{{ $addon->isPandu ? 'Yes' : '' }}</td>
-                    <td>{{ $addon->isBongkarMuat ? 'Yes' : '' }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-
-        <!-- Add more content here as needed -->
-    </div>
+    <p class="data-mandiri">
+        Data Mandiri
+    </p>
+    <p style="padding-top: 4pt; text-indent: 0pt; text-align: left"><br /></p>
+    <table style="border-collapse: collapse; margin-left: 31.8489pt" cellspacing="0">
+        <tr style="height: 14pt">
+            <td class="border1-td" rowspan="2">
+                <p class="s1" style="padding-top: 9pt;padding-left: 17pt;text-indent: 0pt;text-align: left;">
+                    Passing
+                </p>
+            </td>
+            <td class="border2-td" colspan="3">
+                <p class="s1" style="padding-top: 2pt; text-indent: 0pt; text-align: center">
+                    Pandu
+                </p>
+            </td>
+            <td class="border3-td" colspan="3">
+                <p class="s1" style="padding-top: 2pt; text-indent: 0pt; text-align: center">
+                    Bongkar Muat
+                </p>
+            </td>
+            <td class="border1-td" rowspan="2">
+                <p class="s1" style="padding-top: 9pt;padding-left: 10pt;text-indent: 0pt;text-align: left;">
+                    Total Kapal
+                </p>
+            </td>
+        </tr>
+        <tr style="height: 14pt">
+            <td class="border4-td">
+                <p class="s1" style="padding-top: 2pt;padding-left: 12pt;text-indent: 0pt;text-align: left;">
+                    Tervalidasi
+                </p>
+            </td>
+            <td class="border5-td">
+                <p class="s1" style=" padding-top: 2pt; padding-left: 7pt; text-indent: 0pt; text-align: left; ">
+                    Tidak Terjadwal
+                </p>
+            </td>
+            <td class="border6-td">
+                <p class="s1" style=" padding-top: 2pt; padding-left: 22pt; text-indent: 0pt; text-align: left; ">
+                    Terlambat
+                </p>
+            </td>
+            <td class="border7-td">
+                <p class="s1" style=" padding-top: 2pt; padding-left: 10pt; text-indent: 0pt; text-align: left; ">
+                    Tervalidasi
+                </p>
+            </td>
+            <td class="border8-td">
+                <p class="s1" style=" padding-top: 2pt; padding-left: 14pt; text-indent: 0pt; text-align: left; ">
+                    Tidak Terjadwal
+                </p>
+            </td>
+            <td class="border9-td">
+                <p class="s1" style=" padding-top: 2pt; padding-left: 12pt; text-indent: 0pt; text-align: left; ">
+                    Terlambat
+                </p>
+            </td>
+        </tr>
+        <tr style="height: 14pt">
+            <td class="border1-td">
+                <p style="text-indent: 0pt; text-align: left">{{ $summaryData['passing_count'] }}</p>
+            </td>
+            <td class="border4-td">
+                <p style="text-indent: 0pt; text-align: left">{{ $summaryData['pandu_count'] && $summaryData['pandu_count']['tervalidasi']}}</p>
+            </td>
+            <td class="border5-td">
+                <p style="text-indent: 0pt; text-align: left">{{ $summaryData['pandu_count'] && $summaryData['pandu_count']['tidakTerjadwal']}}</p>
+            </td>
+            <td class="border6-td">
+                <p style="text-indent: 0pt; text-align: left">{{ $summaryData['pandu_count'] && $summaryData['pandu_count']['terlambat']}}</p>
+            </td>
+            <td class="border7-td">
+                <p style="text-indent: 0pt; text-align: left">{{ $summaryData['bongkar_muat_count'] && $summaryData['bongkar_muat_count']['tervalidasi']}}</p>
+            </td>
+            <td class="border8-td">
+                <p style="text-indent: 0pt; text-align: left">{{ $summaryData['bongkar_muat_count'] && $summaryData['bongkar_muat_count']['tidakTerjadwal']}}</p>
+            </td>
+            <td class="border9-td">
+                <p style="text-indent: 0pt; text-align: left">{{ $summaryData['bongkar_muat_count'] && $summaryData['bongkar_muat_count']['terlambat']}}</p>
+            </td>
+            <td class="border1-td">
+                <p style="text-indent: 0pt; text-align: left">{{ $summaryData['passing_count'] + $summaryData['pandu_count'] + $summaryData['bongkar_muat_count'] }}</p>
+            </td>
+        </tr>
+    </table>
+    <h1 style=" font-size: 22px; padding-top: 15pt; padding-left: 8pt; text-indent: 0pt; text-align: left; ">
+        List Kapal
+    </h1>
+    <p style="padding-top: 4pt; text-indent: 0pt; text-align: left"><br /></p>
+    <table style="border-collapse: collapse; margin-left: 5.97145pt" cellspacing="0">
+        <tr style="height: 14pt">
+            <td class="border10-td">
+                <p class="s1" style="font-size: 16px; padding-top: 2pt; text-indent: 0pt; text-align: center">
+                    No
+                </p>
+            </td>
+            <td class="border11-td">
+                <p class="s1" style="font-size: 16px; padding-top: 2pt; padding-left: 36pt; text-indent: 0pt; text-align: left;">
+                    Informasi Kapal
+                </p>
+            </td>
+            <td class="border5-td">
+                <p class="s1" style="font-size: 16px;  padding-top: 2pt; padding-left: 8pt; text-indent: 0pt; text-align: left; ">
+                    Pelabukan Asal
+                </p>
+            </td>
+            <td class="border12-td">
+                <p class="s1" style="font-size: 16px;  padding-top: 2pt; padding-left: 6pt; text-indent: 0pt; text-align: left; ">
+                    Pelabuhan Tujuan
+                </p>
+            </td>
+            <td class="border13-td">
+                <p class="s1" style="font-size: 16px; padding-top: 2pt; padding-left: 50pt; text-indent: 0pt; text-align: left; ">
+                    No PKK
+                </p>
+            </td>
+            <td class="border14-td">
+                <p class="s1" style="font-size: 16px;  padding-top: 2pt; padding-left: 20pt; text-indent: 0pt; text-align: left; ">
+                    BUP/TUKS/TERSUS
+                </p>
+            </td>
+            <td class="border1-td">
+                <p class="s1" style="font-size: 16px;  padding-top: 2pt; padding-left: 30pt; text-indent: 0pt; text-align: left; ">
+                    Gambar
+                </p>
+            </td>
+            <td class="border1-td">
+                <p class="s1" style="font-size: 16px; padding-top: 2pt; text-indent: 0pt; text-align: center">
+                    Tarif
+                </p>
+            </td>
+        </tr>
+        @foreach ($addons as $key => $addon)
+        <tr style="height: 14pt">
+            <td class="border10-td">
+                <p class="s2" style="font-size:12px; padding-top: 2pt; text-indent: 0pt; text-align: center">1</p>
+            </td>
+            <td class="border11-td">
+                <p style="font-size:12px; text-indent: 0pt; text-align: left">
+                    Nama Kapal: {{ $addon->aisDataVessel->vessel_name }} <br>
+                    Tipe: {{ $addon->aisDataVessel->vessel_type }} <br>
+                    MMSI: {{ $addon->aisDataVessel->mmsi }} <br>
+                    Perusahaan: {{ $addon->aisDataVessel->nama_perusahaan }}
+                </p>
+            </td>
+            <td class="border5-td">
+                <p style="font-size:12px; text-indent: 0pt; text-align: left">contoh asal</p>
+            </td>
+            <td class="border12-td">
+                <p style="font-size:12px; text-indent: 0pt; text-align: left">contoh tujuan</p>
+            </td>
+            <td class="border13-td">
+                <p style="font-size:12px; text-indent: 0pt; text-align: left">{{ $addon->aisDataVessel->no_pkk }}</p>
+            </td>
+            <td class="border14-td">
+                <p style="font-size:12px; text-indent: 0pt; text-align: left"><br /></p>
+            </td>
+            <td class="border1-td">
+                <p style="font-size:12px; text-indent: 0pt; text-align: left"><br /></p>
+            </td>
+            <td class="border1-td">
+                <p style="font-size:12px; text-indent: 0pt; text-align: left"><br /></p>
+            </td>
+        </tr>
+        @endforeach
+    </table>
 </body>
 
 </html>
