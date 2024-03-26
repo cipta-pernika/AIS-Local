f9bf05c0bb0cc5fb6fc578abd64c2f29457b3b17
ABE <abeabeabeabe444@gmail.com>
2024-03-26 18:36:40 +0700

    🐛

--- app/Models/PanduTidakTerjadwal.php
+++ app/Models/PanduTidakTerjadwal.php
@@ -79,4 +79,9 @@ class PanduTidakTerjadwal extends Model
     {
         return $this->belongsTo(\App\Models\PbkmKegiatanPemanduan::class, 'pbkm_kegiatan_pemanduan_id');
     }
+
+    public function reportGeofenceBongkarMuat(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(\App\Models\ReportGeofenceBongkarMuat::class, 'report_geofence_bongkar_muat_id');
+    }
 }
