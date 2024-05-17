868737790897231490cb02fa3b33ccb6f750306d
ABE <abeabeabeabe444@gmail.com>
2024-05-17 13:43:11 +0700

    🐛

--- database/migrations/2024_05_16_101814_pkk_assign_histories.php
+++ database/migrations/2024_05_16_101814_pkk_assign_histories.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('pkk_assign_histories', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('ais_data_vessel_id')->nullable();
+            $table->string('no_pkk')->nullable();
+            $table->string('no_pkk_assign')->nullable();
+            $table->string('nama_perusahaan')->nullable();
             
             $table->timestamps();
             $table->softDeletes();
