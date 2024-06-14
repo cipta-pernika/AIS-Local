97d79aa2740e754e7d4889340912f17121d6f3e9
ABE <abeabeabeabe444@gmail.com>
2024-06-14 16:02:53 +0700

    🐛

--- resources/views/pdf/reportharian.blade.php
+++ resources/views/pdf/reportharian.blade.php
@@ -6,11 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Bootstrap demo</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        .table-bordered th,
+        .table-bordered td {
+            border: 1px solid #ddd !important
+        }
+    </style>
 </head>
 
 <body>
     <div class="container mt-4">
-        <h1 class="mb-4 text-center">Summary Report</h1>
+        <img class="mb-5 text-center" src="{{asset('images/logo_apl_bjm2.png')}}" />
+        <h1 class="mb-4 mt-1 text-center">Summary Report</h1>
         <table class="table table-bordered">
             <thead>
                 <tr>
