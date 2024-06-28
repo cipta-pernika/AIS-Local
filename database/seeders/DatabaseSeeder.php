c995859863acd75e88dbd056aaa6b5927ef7c524
ABE <abe@pernika.com>
2024-06-28 10:03:33 +0700

    🐛

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,8 +17,8 @@ class DatabaseSeeder extends Seeder
             RolesTableSeeder::class,
             UsersTableSeeder::class,
             BannersTableSeeder::class,
-            BlogCategoriesTableSeeder::class,
-            BlogPostsTableSeeder::class,
+            // BlogCategoriesTableSeeder::class,
+            // BlogPostsTableSeeder::class,
         ]);
 
         Artisan::call('shield:generate --all');
