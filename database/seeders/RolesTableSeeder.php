5e3bdfa011f2fecee7c463a0974499d364f8a14c
ABE <abe@pernika.com>
2024-06-28 14:03:40 +0700

    🐛

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Artisan;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class RolesTableSeeder extends Seeder
 {
@@ -24,5 +27,19 @@ class RolesTableSeeder extends Seeder
                 ]
             );
         }
+
+        Artisan::call('shield:generate --all');
+
+        $role = Role::find(2);
+        $role->givePermissionTo(Permission::all());
+
+        $role = Role::find(3);
+        $includedPermission = ['Impt', 'Inaportnet', 'Pbkm', 'Pelindo'];
+        $permissions = Permission::where(function ($query) use ($includedPermission) {
+            foreach ($includedPermission as $value) {
+                $query->orWhere('name', 'like', '%' . $value . '%');
+            }
+        })->pluck('name')->toArray();
+        $role->givePermissionTo($permissions);
     }
 }
