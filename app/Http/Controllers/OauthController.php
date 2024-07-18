fff305c19e692af4bebe7c24feeb07c1fedc3a13
ABE <abeabeabeabe444@gmail.com>
2024-07-18 21:02:47 +0700

    🐛

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -27,6 +27,23 @@ class OauthController extends Controller
         // auth?state=b41bd5628a2e3895bad456b9c0945a3e
         // &response_type=code&approval_prompt=auto&
         // redirect_uri=https%3A%2F%2Fbackend.sopbuntutksopbjm.com%2Fapi%2Fssocallback%2Fbesopbuntut&client_id=sop-buntut-api
+
+        // https://sso.hubla.dephub.go.id/realms/djpl/protocol/openid-connect/
+        // auth?state=77ad08c0282e54ec7bc7683695ca94f9&response_type=code&approval_prompt=
+        // auto&redirect_uri=https%3A%2F%2Fbackend.sopbuntutksopbjm.com%2Fapi%2Fssocallback%2Fbesopbuntut&client_id=sop-buntut-api
+
+        // {
+        //     "id": "8e1c905aa5724c71a3724ae6bd908c67",
+        //     "created": 1721298078,
+        //     "request_type": "si:r",
+        //     "code_verifier": "be08becacb604eef8b1f31cbef861a8a0984b42607974d8a9588377f5db9d24f651939dfb33240b3b0cc7f9e62bc8a16",
+        //     "authority": "https://sso-dev.hubla.dephub.go.id/realms/djpl",
+        //     "client_id": "sop-buntut-api",
+        //     "redirect_uri": "https://backend.sopbuntutksopbjm.com/api/ssocallback/besopbuntut",
+        //     "scope": "openid email profile",
+        //     "client_secret": "rMBqlBpl3hjDljsjmO2qjE308CTXJdT5",
+        //     "extraTokenParams": {}
+        //   }
     }
 
     public function handleCallback()
