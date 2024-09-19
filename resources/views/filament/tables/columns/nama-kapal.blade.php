f59017790de072e300a21ff3f2e8b559f9cb32a8
ABE <abeabeabeabe444@gmail.com>
2024-09-19 11:21:42 +0700

    🔥

--- resources/views/filament/tables/columns/nama-kapal.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <small style="color: #17a2b8; font-style: italic">Nama Kapal - Tipe Kapal</small>
-    <br />
-    {{ $getRecord()->nama_kapal }}
-    <br />
-    {{ $getRecord()->tipe_kapal }}
-    <br />
-    Kade Tambat : {{ $getRecord()->kade }}
-</div>
