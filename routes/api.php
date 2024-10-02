be22a11c0489eae6b333078a2f940f80f7e432b1
ABE <abe@pernika.com>
2024-10-02 14:22:55 +0700

    🐛

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
-
-use App\Http\Controllers\AisDataController;
+=
 use App\Http\Controllers\Api\DataloggerController;
 use App\Http\Controllers\Api\GeofenceController;
 use App\Http\Controllers\Api\LocationController;
@@ -8,6 +7,7 @@ use App\Http\Controllers\Api\LocationTypeController;
 use App\Http\Controllers\Api\GeofenceTypeController;
 use App\Http\Controllers\Api\PelabuhanController;
 use App\Http\Controllers\Api\SensorController;
+use App\Http\Controllers\Api\AisDataVesselController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -22,6 +22,7 @@ Route::apiResource('geofences', GeofenceController::class);
 Route::apiResource('geofence_types', GeofenceTypeController::class);
 Route::apiResource('pelabuhans', PelabuhanController::class);
 Route::apiResource('sensors', SensorController::class);
+Route::apiResource('ais_data_vessels', AisDataVesselController::class);
 Route::get('aisdataunique', [AisDataController::class, 'unique']);
 
 
