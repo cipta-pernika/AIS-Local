f0e2667e32656de6f5f9c953a14c89814a77c8bc
sen3064 <sensuryo@gmail.com>
2024-10-16 16:52:27 +0700

    🐛

--- resources/views/pdf/historyreport.blade.php
+++ resources/views/pdf/historyreport.blade.php
@@ -1,19 +1,27 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+        crossorigin="anonymous" />
     <style>
+        body {
+            font-family: 'Arial', sans-serif;
+        }
 
-        .table-bordered thead {
+        .table-bordered thead,
+        .table-bordered tbody {
             border-color: transparent !important;
         }
+
         .table-bordered th {
-            background-color: #5498fe;
+            background-color: #183f79;
             color: #fff;
         }
 
@@ -28,8 +36,17 @@
         .table-bordered th,
         .table-bordered td {
             /* border: 1px solid #ddd !important; */
-            padding: 6px 4px;
+            padding: 8px;
             font-size: 12px;
+        }
+
+        .table-bordered tbody tr:nth-child(odd) td {
+            background-color: #ffffff;
+            /* or any other color you prefer */
+        }
+
+        .table-bordered tbody tr:nth-child(even) td {
+            background-color: #ebebeb;
         }
 
         .page-break {
@@ -65,22 +82,23 @@
                 </thead>
                 <tbody>
                     @foreach ($data as $item)
-                        <tr>
-                            <td>{{ $item->sensor_data_id }}</td>
-                            <td>{{ $item->vessel_id }}</td>
-                            <td>{{ $item->latitude }}</td>
-                            <td>{{ $item->longitude }}</td>
-                            <td>{{ $item->speed }}</td>
-                            <td>{{ $item->course }}</td>
-                            <td>{{ $item->heading }}</td>
-                            <td>{{ $item->navigation_status }}</td>
-                            <td>{{ $item->turning_rate }}</td>
-                            <td>{{ $item->turning_direction }}</td>
-                            <td>{{ $item->timestamp }}</td>
-                            <td>{{ $item->distance }}</td>
-                        </tr>
-                    @endforeach
                     <tr>
+                        <td>{{ $item->sensor_data_id }}</td>
+                        <td>{{ $item->vessel_id }}</td>
+                        <td>{{ $item->latitude }}</td>
+                        <td>{{ $item->longitude }}</td>
+                        <td>{{ $item->speed }}</td>
+                        <td>{{ $item->course }}</td>
+                        <td>{{ $item->heading }}</td>
+                        <td>{{ $item->navigation_status }}</td>
+                        <td>{{ $item->turning_rate }}</td>
+                        <td>{{ $item->turning_direction }}</td>
+                        <td>{{ $item->timestamp }}</td>
+                        <td>{{ $item->distance }}</td>
+                    </tr>
+
+                    @endforeach
+                    <tr></tr>
                 </tbody>
             </table>
         </div>
