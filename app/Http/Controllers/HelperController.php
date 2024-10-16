e631667dc720cb9d04cbb1b3e81a2d721e510251
sen3064 <sensuryo@gmail.com>
2024-10-16 16:58:43 +0700

    🐛

--- app/Http/Controllers/HelperController.php
+++ app/Http/Controllers/HelperController.php
@@ -991,9 +991,9 @@ class HelperController extends Controller
             
             $aisData = $query->get();
             // Tambahkan hardcode status AIS ke setiap item dalam koleksi
-            $aisData->each(function ($item) {
-                $item->ais_status = 'On';
-            });
+            // $aisData->each(function ($item) {
+            //     $item->ais_status = 'On';
+            // });
 
             Cache::put($cacheKey, $aisData, 10);
         }
@@ -1014,9 +1014,9 @@ class HelperController extends Controller
             ->get();
 
         // Tambahkan hardcode status AIS ke setiap item dalam koleksi
-        $aisData->each(function ($item) {
-            $item->ais_status = 'On';
-        });
+        // $aisData->each(function ($item) {
+        //     $item->ais_status = 'On';
+        // });
         return response()->json([
             'success' => true,
             'message' => $aisData,
