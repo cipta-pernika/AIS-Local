70837df1c4c6a670f670826fdb319a0cd8630f77
sen3064 <sensuryo@gmail.com>
2024-10-17 10:03:01 +0700

    🐛

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -75,7 +75,7 @@ class ExportController extends Controller
                 }
             })
             ->select('ais_data_vessels.*', 'ais_data_positions.timestamp') // Pilih kolom yang diperlukan
-            // ->take(10000) // Batasi hingga 100 baris
+            ->take(10) // Batasi hingga 100 baris
             ->get();
 
         if ($format === 'pdf') {
