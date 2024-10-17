a3bdf996047727c2357618d4878edb7387ba92b9
sen3064 <sensuryo@gmail.com>
2024-10-17 17:10:50 +0700

    🐛

--- app/Http/Controllers/Api/CctvController.php
+++ app/Http/Controllers/Api/CctvController.php
@@ -14,25 +14,25 @@ class CctvController extends Controller
     /**
      * Display a listing of the resource.
      */
-    // public function index(Request $request)
-    // {
-    //     $cctvs = Cctv::paginate();
-
-    //     return CctvResource::collection($cctvs);
-    // }
     public function index(Request $request)
-{
-    $query = Cctv::query();
-
-    if (isset($request->terminal_id)) {
-        $query->whereIn('terminal_id', $request->terminal_id);
+    {
+        // Retrieve terminal_ids from the request
+        $terminalIds = $request->query('terminal_id', []);
+    
+        // Filter by terminal_id if provided
+        $query = Cctv::query();
+    
+        if (!empty($terminalIds)) {
+            // If multiple terminal_ids are provided, filter accordingly
+            $query->whereIn('terminal_id', $terminalIds);
+        }
+    
+        // Paginate the results
+        $cctvs = $query->paginate();
+    
+        return CctvResource::collection($cctvs);
     }
-
-    $cctvs = $query->paginate();
-
-    return CctvResource::collection($cctvs);
-}
-
+    
     /**
      * Store a newly created resource in storage.
      */
