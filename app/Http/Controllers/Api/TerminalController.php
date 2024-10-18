d364de697aafd6ca291c26582b937e1b4b04a42a
sen3064 <sensuryo@gmail.com>
2024-10-18 15:12:23 +0700

    filter

--- app/Http/Controllers/Api/TerminalController.php
+++ app/Http/Controllers/Api/TerminalController.php
@@ -24,8 +24,11 @@ class TerminalController extends Controller
 
         // Join dengan tabel dataloggers dan sensors, dan pilih kolom status dari sensors
         $query->join('dataloggers', 'terminals.pelabuhan_id', '=', 'dataloggers.pelabuhan_id')
-              ->join('sensors', 'dataloggers.pelabuhan_id', '=', 'sensors.datalogger_id')
-              ->select('terminals.*', 'sensors.status');
+            ->join('sensors', 'dataloggers.pelabuhan_id', '=', 'sensors.datalogger_id')
+            ->select('terminals.*', 'sensors.status');
+        if ($request->has('status')) {
+            $query->where('sensors.status', $request->input('status'));
+        }
 
         $terminals = $query->paginate();
 
