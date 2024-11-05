86bd0f35cc5ac1a2a199c31a519c0030c9766455
Your Name <you@example.com>
2024-11-05 12:31:32 +0700

    🐛

--- app/Http/Controllers/PermissionController.php
+++ app/Http/Controllers/PermissionController.php
@@ -10,8 +10,23 @@ class PermissionController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $limit = $request->input('limit', 15); // Default to 15 items per page
-        $permissions = Permission::paginate($limit);
+        $limit = $request->input('limit', 15);
+        $permissions = Permission::whereIn('name', [
+            'show_notifikasi',
+            'show_playback',
+            'show_cctv',
+            'is_controlled_ptz',
+            'show_legend',
+            'show_poi',
+            'show_layer_map',
+            'show_server_status',
+            'show_report',
+            'export_report',
+            'show_user_management',
+            'add_user',
+            'delete_user',
+            'edit_user'
+        ])->paginate($limit);
 
         return PermissionResource::collection($permissions);
     }
