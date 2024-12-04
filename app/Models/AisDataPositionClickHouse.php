1e1e40ecca6376fda0fcc8d6747663c2ce10149b
ABE <abeabeabeabe444@gmail.com>
2024-12-04 11:44:39 +0700

    ✨ clickhouse

---
+++ app/Models/AisDataPositionClickHouse.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use PhpClickHouseLaravel\BaseModel;
+
+class AisDataPositionClickHouse extends BaseModel
+{
+    use HasFactory;
+
+    protected $table = 'ais_data_positions';
+}
