9b94b1042b1365e5ee384557f153c239e3654634
ABE <abeabeabeabe444@gmail.com>
2025-02-04 11:39:08 +0700

    🔥

--- database/migrations/2024_12_04_134856_ais_data_position_clickhouse.php
+++ database/migrations/2024_12_04_134856_ais_data_position_clickhouse.php
@@ -11,28 +11,28 @@ return new class extends \PhpClickHouseLaravel\Migration
      */
     public function up(): void
     {
-        static::write('
-            CREATE TABLE ais_data_position_clickhouse (
-                id UInt64,
-                sensor_data_id UInt64,
-                vessel_id UInt64,
-                latitude Decimal(10, 7),
-                longitude Decimal(10, 7),
-                speed Decimal(8, 2),
-                course Nullable(UInt16),
-                heading Nullable(UInt16),
-                navigation_status Nullable(String),
-                turning_rate Nullable(Int32),
-                turning_direction Nullable(Int32),
-                timestamp DateTime,
-                distance Nullable(Decimal(8, 2)),
-                is_inside_geofence Int8 DEFAULT 0,
-                is_geofence Int8 DEFAULT 0,
-                created_at DateTime
-            )
-            ENGINE = MergeTree()
-            ORDER BY (id)
-        ');
+        // static::write('
+        //     CREATE TABLE ais_data_position_clickhouse (
+        //         id UInt64,
+        //         sensor_data_id UInt64,
+        //         vessel_id UInt64,
+        //         latitude Decimal(10, 7),
+        //         longitude Decimal(10, 7),
+        //         speed Decimal(8, 2),
+        //         course Nullable(UInt16),
+        //         heading Nullable(UInt16),
+        //         navigation_status Nullable(String),
+        //         turning_rate Nullable(Int32),
+        //         turning_direction Nullable(Int32),
+        //         timestamp DateTime,
+        //         distance Nullable(Decimal(8, 2)),
+        //         is_inside_geofence Int8 DEFAULT 0,
+        //         is_geofence Int8 DEFAULT 0,
+        //         created_at DateTime
+        //     )
+        //     ENGINE = MergeTree()
+        //     ORDER BY (id)
+        // ');
     }
 
     /**
@@ -40,6 +40,6 @@ return new class extends \PhpClickHouseLaravel\Migration
      */
     public function down(): void
     {
-        static::write('DROP TABLE ais_data_position_clickhouse');
+        // static::write('DROP TABLE ais_data_position_clickhouse');
     }
 };
