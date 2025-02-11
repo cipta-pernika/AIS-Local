f1cb2e6550f13320ac255a9c10a97d73d635bda0
ABE <abeabeabeabe444@gmail.com>
2025-02-11 13:31:08 +0700

    🐛

--- database/migrations/2023_05_22_092956_sensor_datas.php
+++ database/migrations/2023_05_22_092956_sensor_datas.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -20,6 +21,23 @@ return new class extends Migration
             $table->timestamps();
 
             $table->index('sensor_id');
+        });
+
+        $currentMonth = date('Y_m');
+        Schema::create("sensor_datas_{$currentMonth}", function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('sensor_id');
+            $table->foreign('sensor_id')->references('id')->on('sensors');
+            $table->text('payload');
+            $table->timestamp('timestamp');
+            $table->timestamps();
+
+            $table->index('sensor_id');
+        });
+
+        // Move data from sensor_datas to sensor_datas_{$currentMonth}
+        DB::table('sensor_datas')->whereMonth('timestamp', date('m'))->get()->each(function ($data) use ($currentMonth) {
+            DB::table("sensor_datas_{$currentMonth}")->insert((array) $data);
         });
     }
 
