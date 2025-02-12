ccd316bb5b639fe640e370650a2a96269487695c
ABE <abeabeabeabe444@gmail.com>
2025-02-12 19:52:36 +0700

    🐛

--- database/seeders/EventSeeder.php
+++ database/seeders/EventSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Event;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EventSeeder extends Seeder
 {
@@ -13,54 +14,42 @@ class EventSeeder extends Seeder
      */
     public function run(): void
     {
-        Event::create([
-            'name' => 'Normal Positions',
-            'threshold' => 10,
-        ]);
+        if (DB::getDriverName() == 'mongodb') {
+            $events = [
+                ['name' => 'Normal Positions', 'threshold' => 10],
+                ['name' => 'ON AIS', 'threshold' => 10],
+                ['name' => 'OFF AIS', 'threshold' => 10],
+                ['name' => 'ON AIS Receiver Coverage', 'threshold' => 10],
+                ['name' => 'OFF AIS Receiver Coverage', 'threshold' => 10],
+                ['name' => 'Entry coverage AIS (new detected)', 'threshold' => 10],
+                ['name' => 'Navigation status changes', 'threshold' => 10],
+                ['name' => 'Potential Collision', 'threshold' => 10],
+                ['name' => 'Enter Geofence', 'threshold' => 10],
+                ['name' => 'Exit Geofence', 'threshold' => 10],
+            ];
 
-        Event::create([
-            'name' => 'ON AIS',
-            'threshold' => 10,
-        ]);
+            foreach ($events as $event) {
+                DB::connection('mongodb')
+                    ->selectCollection('events')
+                    ->insertOne($event);
+            }
+        } else {
+            $events = [
+                ['name' => 'Normal Positions', 'threshold' => 10],
+                ['name' => 'ON AIS', 'threshold' => 10],
+                ['name' => 'OFF AIS', 'threshold' => 10],
+                ['name' => 'ON AIS Receiver Coverage', 'threshold' => 10],
+                ['name' => 'OFF AIS Receiver Coverage', 'threshold' => 10],
+                ['name' => 'Entry coverage AIS (new detected)', 'threshold' => 10],
+                ['name' => 'Navigation status changes', 'threshold' => 10],
+                ['name' => 'Potential Collision', 'threshold' => 10],
+                ['name' => 'Enter Geofence', 'threshold' => 10],
+                ['name' => 'Exit Geofence', 'threshold' => 10],
+            ];
 
-        Event::create([
-            'name' => 'OFF AIS',
-            'threshold' => 10,
-        ]);
-
-        Event::create([
-            'name' => 'ON AIS Receiver Coverage',
-            'threshold' => 10,
-        ]);
-
-        Event::create([
-            'name' => 'OFF AIS Receiver Coverage',
-            'threshold' => 10,
-        ]);
-
-        Event::create([
-            'name' => 'Entry coverage AIS (new detected)',
-            'threshold' => 10,
-        ]);
-
-        Event::create([
-            'name' => 'Navigation status changes',
-            'threshold' => 10,
-        ]);
-
-        Event::create([
-            'name' => 'Potential Collision',
-            'threshold' => 10,
-        ]);
-
-        Event::create([
-            'name' => 'Enter Geofence',
-            'threshold' => 10,
-        ]);
-
-        Event::create([
-            'name' => 'Exit Geofence',
-            'threshold' => 10,
-        ]);
+            foreach ($events as $event) {
+                Event::create($event);
+            }
+        }
     }
 }
--- database/seeders/PelabuhanSeeder.php
+++ database/seeders/PelabuhanSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Pelabuhan;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PelabuhanSeeder extends Seeder
 {
@@ -13,12 +14,20 @@ class PelabuhanSeeder extends Seeder
      */
     public function run(): void
     {
-        Pelabuhan::create([
+        $pelabuhan = [
             'name' => 'Banjarmasin',
             'un_locode' => 'IDBDJ',
             'latitude' => -3.3265939,
             'longitude' => 114.559263,
             'address' => 'Jl. Barito Hilir Trisakti No. 6, Banjarmasin, Kalimantan Selatan - 70119'
-        ]);
+        ];
+
+        if (DB::getDriverName() == 'mongodb') {
+            DB::connection('mongodb')
+                ->selectCollection('pelabuhans')
+                ->insertOne($pelabuhan);
+        } else {
+            Pelabuhan::create($pelabuhan);
+        }
     }
 }
