c70e347974b431f54d5bb4d452be923893d068b5
ABE <abeabeabeabe444@gmail.com>
2025-02-18 15:14:10 +0700

    mongodb

--- config/database.php
+++ config/database.php
@@ -3,38 +3,34 @@
 use Illuminate\Support\Str;
 
 return [
-
     /*
-    |--------------------------------------------------------------------------
-    | Default Database Connection Name
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify which of the database connections below you wish
-    | to use as your default connection for all database work. Of course
-    | you may use many connections at once using the Database library.
-    |
-    */
-
+     * |--------------------------------------------------------------------------
+     * | Default Database Connection Name
+     * |--------------------------------------------------------------------------
+     * |
+     * | Here you may specify which of the database connections below you wish
+     * | to use as your default connection for all database work. Of course
+     * | you may use many connections at once using the Database library.
+     * |
+     */
     'default' => env('DB_CONNECTION', 'mysql'),
 
     /*
-    |--------------------------------------------------------------------------
-    | Database Connections
-    |--------------------------------------------------------------------------
-    |
-    | Here are each of the database connections setup for your application.
-    | Of course, examples of configuring each database platform that is
-    | supported by Laravel is shown below to make development simple.
-    |
-    |
-    | All database work in Laravel is done through the PHP PDO facilities
-    | so make sure you have the driver for your particular database of
-    | choice installed on your machine before you begin development.
-    |
-    */
-
+     * |--------------------------------------------------------------------------
+     * | Database Connections
+     * |--------------------------------------------------------------------------
+     * |
+     * | Here are each of the database connections setup for your application.
+     * | Of course, examples of configuring each database platform that is
+     * | supported by Laravel is shown below to make development simple.
+     * |
+     * |
+     * | All database work in Laravel is done through the PHP PDO facilities
+     * | so make sure you have the driver for your particular database of
+     * | choice installed on your machine before you begin development.
+     * |
+     */
     'connections' => [
-
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DATABASE_URL'),
@@ -42,7 +38,6 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
@@ -62,7 +57,6 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
@@ -77,7 +71,6 @@ return [
             'search_path' => 'public',
             'sslmode' => 'prefer',
         ],
-
         'sqlsrv' => [
             'driver' => 'sqlsrv',
             'url' => env('DATABASE_URL'),
@@ -92,42 +85,46 @@ return [
             // 'encrypt' => env('DB_ENCRYPT', 'yes'),
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
-
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI', 'mongodb://localhost:27017'),
+            'database' => env('DB_DATABASE', 'homestead'),
+        ],
+        'mongodb2' => [
+            'driver' => 'mongodb',
+            'dsn' => env('DB_SECOND_URI', 'mongodb://localhost:27017'),
+            'database' => env('DB_SECOND_DATABASE', 'second_database'),
+        ],
     ],
 
     /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run in the database.
-    |
-    */
-
+     * |--------------------------------------------------------------------------
+     * | Migration Repository Table
+     * |--------------------------------------------------------------------------
+     * |
+     * | This table keeps track of all the migrations that have already run for
+     * | your application. Using this information, we can determine which of
+     * | the migrations on disk haven't actually been run in the database.
+     * |
+     */
     'migrations' => 'migrations',
 
     /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as APC or Memcached. Laravel makes it easy to dig right in.
-    |
-    */
-
+     * |--------------------------------------------------------------------------
+     * | Redis Databases
+     * |--------------------------------------------------------------------------
+     * |
+     * | Redis is an open source, fast, and advanced key-value store that also
+     * | provides a richer body of commands than a typical key-value system
+     * | such as APC or Memcached. Laravel makes it easy to dig right in.
+     * |
+     */
     'redis' => [
-
         'client' => env('REDIS_CLIENT', 'phpredis'),
-
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
-
         'default' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -136,7 +133,6 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
-
         'cache' => [
             'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
@@ -145,7 +141,5 @@ return [
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
-
     ],
-
 ];
